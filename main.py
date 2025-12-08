da68b36b86e1faeac801aa37bea96ab7446f16a1
hojat72elect <hojat72elect@gmail.com>
2025-12-08 03:54:58 -0500

    Cleaned up.

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def main():
     soup = BeautifulSoup(response.text, "lxml")
 
     for card in soup.select(".card-item"):
-            img = card.select_one(".card-item__img").select_one("img").get("src")
-            link = card.select_one(".card-item__link").get("href")
-            title = card.select_one(".card-item__title").get_text(strip=True)
+        img = card.select_one(".card-item__img").select_one("img").get("src")
+        link = card.select_one(".card-item__link").get("href")
+        title = card.select_one(".card-item__title").get_text(strip=True)
 
-            print(str(link))
-            print(str(title))
-            print(str(img))
-            print()
+        print(str(link))
+        print(str(title))
+        print(str(img))
+        print()
 
 if __name__ == "__main__":
     main()
