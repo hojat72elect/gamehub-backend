1a319efdb211776d716897f34ff249c49793a51b
hojat72elect <hojat72elect@gmail.com>
2025-12-08 04:06:32 -0500

    Re-wrote the script for the first 20 pages of news articles (GameSpot).

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
-    url = "https://www.gamespot.com/news/"
-    response = requests.get(url)
+    total_pages = 20
+    url = "https://www.gamespot.com/news/?page="
 
-    soup = BeautifulSoup(response.text, "lxml")
-
-    for card in soup.select(".card-item"):
+    for page_number in range(total_pages):
+        response = requests.get(f"{url}{page_number}")
+        soup = BeautifulSoup(response.text, "lxml")
+        for card in soup.select(".card-item"):
             img = card.select_one(".card-item__img").select_one("img").get("src")
             link = card.select_one(".card-item__link").get("href")
             title = card.select_one(".card-item__title").get_text(strip=True)
 
-            print(str(link))
+            print(f"https://www.gamespot.com{str(link)}")
             print(str(title))
-            print(str(img))
-            print()
+            print(f"{str(img)}\n")
+
 
 if __name__ == "__main__":
     main()
